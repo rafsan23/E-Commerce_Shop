9f377db804884c32a83a525be579065d95bab368
rafsan23 <rafsanraiyan00@gmail.com>
2023-09-09 14:38:48 +0600

    14th

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -27,7 +27,24 @@ class HomeController extends Controller
             $usertype = Auth::user()->usertype;
 
             if ($usertype == '1') {
-                return view('admin.home');
+                $total_product=product::all()->count();
+                $total_order=order::all()->count();
+                $total_user=user::all()->count();
+
+                $order=order::all();
+                $total_revenue=0;
+
+                foreach($order as $order)
+                {
+                    $total_revenue=$total_revenue + $order->price;
+                }
+
+                $total_delivered=order::where('delivery_status','=','delivered')->get()->count();
+
+                $total_processing=order::where('delivery_status','=','processing')->get()->count();
+
+
+                return view('admin.home',compact('total_product','total_order','total_user','total_revenue','total_delivered','total_processing'));
             } else {
                 $products = Product::paginate(10);
                 return view('home.userpage', compact('products'));
